1bd798d300476da4083f3fc71f649278c5f94773
amanullhazoha <amanullhazoha3784@gmail.com>
2022-04-05 17:52:11 +0600

    Add filter and search feature.

---
+++ src/modules/user/home/components/filterbar.component.js
@@ -0,0 +1,58 @@
+import { Formik, Form, Field } from "formik";
+
+import classes from "../style/filterbar.module.scss";
+
+const LAUNCH_YEARS = ["All", "Less Than 1990", "1991-1995", "1996-2000", "2001-2005", "2006-2010", "2011-2015", "2016-2020", "Greater Than 2020" ]
+
+const Filterbar = ({ handleFilterUpcoming, handleFilterLaunchYear, handleSearch }) => {
+    return (
+        <div className={classes.filterbar}>
+            <div className={classes.filter}>
+                <div className={classes.filterUpcoming}>
+                    <label htmlFor={classes.upcoming}>Is Upcoming?</label>
+                    <select id={classes.upcoming} name="upcoming" onChange={handleFilterUpcoming}>
+                        <option value="no">No</option>
+                        <option value="yes">Yes</option>
+                    </select>
+                </div>
+
+                <div className={classes.filterYear}>
+                    <label htmlFor="launchYear">Launch Year?</label>
+                    <select 
+                        id="launchYear" 
+                        className={classes.selectYear} 
+                        name="launchYear" 
+                        onChange={handleFilterLaunchYear}
+                    >
+                        {LAUNCH_YEARS.map(year => (
+                            <option value={year} key={year}>{year}</option>
+                        ))}
+                    </select>
+                </div>
+            </div>
+
+            <div className={classes.search}>
+                <Formik
+                    initialValues={{ search: "" }}
+                    onSubmit={(value, action) => {
+                        handleSearch(value.search)
+                        action.setSubmitting(false);
+                    }}
+                >
+                    {formikProps => (
+                        <Form onSubmit={formikProps.handleSubmit}>
+                            <Field 
+                                placeholder="Search for rocket"
+                                name="search"
+                            />
+
+                            <button type="submit">Search</button>
+                        </Form>
+                    )}
+                </Formik>
+            </div>
+        </div>
+    );
+}
+ 
+export default Filterbar;
--- src/modules/user/home/components/main-section.component.js
+++ src/modules/user/home/components/main-section.component.js
@@ -1,25 +1,85 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Container, Row } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 
 import classes from "../style/main-section.module.scss";
 import { getLaunchRockets } from "../home.actions";
 import Rocket from "./rocket.component";
+import Filterbar from "./filterbar.component";
 
 const MainSection = () => {
     const dispatch = useDispatch();
 
+    const [searchValue, setSearchValue] = useState("");
+    const [upcoming, setUpcoming] = useState("no");
+    const [launchYear, setLaunchYear] = useState("All");
     const launchRockets = useSelector(state => state.homeReducer.launchRockets);
+
+    const handleSearch = (value) => {
+        setSearchValue(value);
+    }
+
+    const handleFilterUpcoming = (event) => {
+        setUpcoming(event.target.value);
+    }
+
+    const handleFilterLaunchYear = (event) => {
+        setLaunchYear(event.target.value);
+    }
+
+    const searchingData = () => {
+        if(searchValue) {
+            return launchRockets.filter(
+                launchRocket => launchRocket.rocket.rocket_name.toLowerCase() === searchValue.toLowerCase()
+            )
+        } else {
+            return launchRockets;
+        }
+    }
+
+    const filteringData = (datas) => {
+        const filterUpcoming = () => {
+            if(upcoming === "yes") {
+                return datas.filter(data => data.upcoming === true)
+            } else {
+                return datas.filter(data => data.upcoming !== true)
+            }
+        }
+
+        const filterYear = () => {
+            if(launchYear === "All") {
+                return filterUpcoming();
+            } else if(launchYear === "Less Than 1990") {
+                return filterUpcoming().filter(data => Number(data.launch_year) < 1990);
+            } else if (launchYear === "Greater Than 2020") {
+                return filterUpcoming().filter(data => Number(data.launch_year) > 2020);
+            } else {
+                const [start, end] = launchYear.split("-");
+                return filterUpcoming().filter(data => Number(data.launch_year) >= start && Number(data.launch_year) <= end);
+            }
+        }
+
+        return filterYear();
+    }
 
     useEffect(() => {
         dispatch(getLaunchRockets())
     }, [])
 
+    const searchedData = searchingData();
+    const filteredData = filteringData(searchedData);
+
     return (
         <main id={classes.mainSection}>
             <Container>
+                <Filterbar 
+                    handleSearch={handleSearch}
+                    handleFilterUpcoming={handleFilterUpcoming}
+                    handleFilterLaunchYear={handleFilterLaunchYear}
+                />
+
                 <Row>
-                    {launchRockets.map(launchRocket => (
+                    {filteredData.map(launchRocket => (
                         <Rocket 
                             key={launchRocket.flight_number}
                             rocketImage={launchRocket.links.mission_patch_small}
